d6d7dd20f6b84159d4d190577587da637677c078
christopher-buss <christopher.buss+github@protonmail.com>
2025-10-21 22:36:20 +0100

    refactor(commands): use wsl detection for rojo server execution

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -1,10 +1,14 @@
+import { isWsl } from "src/utils/is-wsl";
+
 import { run } from "../utils/run";
 
 export const COMMAND = "serve";
 export const DESCRIPTION = "Start the Rojo development server";
 
 export async function action(): Promise<void> {
-	await run("rojo", ["serve"], {
+	const rojo = isWsl() ? "rojo.exe" : "rojo";
+
+	await run(rojo, ["serve"], {
 		spinnerMessage: "Starting Rojo server...",
 	});
 }
